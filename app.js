e2d9a0e749bae3c847cb9b14ef83551b19d9a87d
Andrew Khor <akhor93@gmail.com>
2014-02-06 22:37:32 -0800

    added search routing

--- app.js
+++ app.js
@@ -65,12 +65,14 @@ if ('development' == app.get('env')) {
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var search = require('./routes/search');
 
 // Add routes here
 app.get('/', index.home);
 app.get('/trips', index.trips);
 app.get('/user', users.show);
 app.get('/followers', index.followers);
+app.get('/search', search.search);
 
 // Example route
 // app.get('/users', user.list);
---
+++ routes/search.js
@@ -0,0 +1,3 @@
+exports.search = function(req, res){
+  res.render('search/search');
+};
