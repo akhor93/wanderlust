d424033d43c3a87d559366f1c5768fb8a0d27d58
Andrew Khor <akhor93@gmail.com>
2014-02-10 17:43:14 -0800

    changed account manager to use mongodb

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var helpers = require('./lib/helpers');
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-// var uristring =
-// process.env.MONGOLAB_URI ||
-// process.env.MONGOHQ_URL ||
-// 'mongodb://localhost/database';
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/database';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
@@ -23,13 +23,13 @@ var theport = process.env.PORT || 5000;
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
-// var db = mongoose.connect(uristring, function (err, res) {
-//   if (err) {
-//   console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//   } else {
-//   console.log ('Succeeded connected to: ' + uristring);
-//   }
-// });
+var db = mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 // Bootstrap models
 var models_path = __dirname + '/models'
