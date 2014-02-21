57238ec3a2780c3cb9cdd3490ae5358af3169416
Andrew Khor <akhor93@gmail.com>
2014-02-20 18:35:30 -0800

    get session data is working now. used callbacks

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,8 @@
 var SH = require("../lib/session_helper");
-var async = require('async');
+// var async = require('async');
 
 exports.home = function(req, res){
-	async.parallel([
-		function(cb) {
-			var data = SH.getSessionData(req.session.user, true);
-			console.log(data);
-			cb(null, data);
-		}
-	], function(err, results) {
-		console.log(results);
-		res.render('index', results);
-	})
-
+	SH.getSessionData(req.session.user, true, function(data) {
+		res.render('index', data);
+	});
 };
-
-exports.trips = function(req, res){
-  data = {};
-	data = SH.getSessionData(req.session.user);
-	res.render('trips', data);
-};
-
-exports.followers = function(req, res){
-  data = {};
-	data = SH.getSessionData(req.session.user);
-	res.render('followers', data);
-};
-
-exports.incrementLikes = function(req, res) {
-	console.log("reached controller");
-	var tripID = req.params.id;
-	console.log(tripID);
-	var newNum = data['trips'][tripID]['num_likes'] + 1;
-	data['trips'][tripID]['num_likes'] = newNum;
-	var dataToPass = {
-		num_likes: newNum,
-		trip_id: tripID
-	}
-	res.json(dataToPass);
-}
