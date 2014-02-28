d08c3a8f5a7bc1f6ac116cf6541d2c18557457b0
Lucy Wang <lucywang@stanford.edu>
2014-02-28 13:48:22 -0800

    can click on profile image to change profile picture

--- routes/sessions.js
+++ routes/sessions.js
@@ -35,11 +35,13 @@ exports.login = function(req, res) {
         User.findOne({username: req.param('username')}).lean().exec(function(err, user) {
           user.showPicUpload = true;
           req.session.user = user;
+          // res.redirect('/user/' + user._id);
           res.send("ok", 200);
         });
       }
       else {
         req.session.user = user;
+        // res.redirect('/user/' + user._id);        
         res.send("ok", 200);
       }
     }
@@ -54,6 +56,7 @@ exports.signout = function(req, res) {
     res.clearCookie('user');
     res.clearCookie('pass');
     req.session.destroy(function(e) {
+      // res.redirect('/');
       res.send('ok', 200);
     });
   }
