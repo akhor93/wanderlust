bd6071999b7586894373c7aca7be68eb342dd01c
Andrew Khor <akhor93@gmail.com>
2014-02-28 03:53:04 -0800

    fixed the weird error that Adrian was getting. Deals with the number of tags being created. 1 is array of string. more than 1 is array of array of strings

--- routes/tag.js
+++ routes/tag.js
@@ -6,20 +6,40 @@ var User = mongoose.model('User');
 var Trip = mongoose.model('Trip');
 var Tag = mongoose.model('Tag');
 
-exports.create = function(req, res) {
-
-}
-
 exports.show = function(req, res) {
 	var tagText = req.params.id;
 	SH.getSessionData(req.session.user, function(data) {
-		Tag.find({text: tagText})
-    .populate('trips')
+    
+  	Tag.findOne({text: tagText})
+    .lean()
     .exec(function(err, tag) {
-    	if(err) console.log(err);
-    	data.tag = tag[0];
-    	console.log(data);
-			res.render('tags/show', data);
+    	if(err) return console.log(err);
+      var asyncfunctions = [];
+      var i;
+      for(i = 0; i < tag.trips.length; i++) {
+        var func = function(cb) {
+          var id = tag.trips[i];
+          Trip.findById(id)
+          .populate('user tags likes favorites')
+          .exec(function(err, trip) {
+            i++;
+            cb(null, trip);
+          });
+        };
+        asyncfunctions.push(func);
+      }
+      i = 0;
+      async.series(asyncfunctions, function(err, results) {
+        for(var i = 0; i < results.length; i++) {
+          tag.trips[i] = results[i];
+        }
+        data.tag = tag;
+        console.log(data);
+        res.render('tags/show', data);
+      });
+    	
+    	
+			
     });
   });
 }
