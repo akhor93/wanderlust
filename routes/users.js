4988c0f6513293702c655c23a9dff65b29fa4c15
Lucy Wang <lucywang@stanford.edu>
2014-02-28 13:54:47 -0800

    fixed site crash when logging out on edit account page

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,24 @@ var CT = require("../modules/country-list");
 var Trip = mongoose.model('Trip');
 
 exports.edit = function(req, res) {
-  SH.getSessionData(req.session.user, function(data) {
-    User.findOne({_id: req.session.user._id}, function(err, user) {
-      if(err) {
-        console.log("Could not find user: " + err);
-        res.redirect('/');
-      }
-      else {
-        data.user = user;
-        data.countries = CT;
-        res.render('users/edit', data);
-      }
-    });
-  });
+  if (req.session.user) {
+    SH.getSessionData(req.session.user, function(data) {
+      User.findOne({_id: req.session.user._id}, function(err, user) {
+        if(err) {
+          console.log("Could not find user: " + err);
+          res.redirect('/');
+        }
+        else {
+          data.user = user;
+          data.countries = CT;
+          res.render('users/edit', data);
+        }
+      });
+    }); 
+  } else {
+    res.redirect('/');
+  }
+
 }
 
 exports.show = function(req, res) {
